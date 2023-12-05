ba1d09547b4d965bb463ed5d23b276546fd416e8
caleb-sudo <123672600+caleb-sudo@users.noreply.github.com>
2023-12-04 20:08:10 -0700

    Update main.c

--- main.c
+++ main.c
@@ -11,6 +11,7 @@
 #define	max_points	100
 #define max_lines	50
 #define max_chars	20
+#define GraphSpeed	7.0f
 
 typedef struct Points {
 	int radius;
@@ -46,7 +47,9 @@ static void Draw(Camera2D camera, Rectangle textbox);
 static void UpdateDrawFrame(Camera2D camera, Rectangle textbox);
 static void Unload();
 
-bool textBox_active;
+bool textBox_active = false;
+bool graph = false;
+bool start = true;
 
 int main(Camera2D camera, Rectangle textbox) {
 	InitWindow(screenWidth, screenHeight, "your calculator");
@@ -64,84 +67,107 @@ static void Init(void) {
 	camera.zoom = 1.0f;
 
 	Rectangle textbox = { screenWidth/2.0f - 100, 180, 225, 50 };
-
+	
 	for (int i = 0; i < max_lines; i++) {
-		lines[i].position = (Vector2) {   };
+		lines[i].position = (Vector2) { 0, y-int };
 		lines[i].active = false;
 	}
 	for (int i = 0; i < max_points; i++) {
 		points[i].active = false;
 		points[i].radius = 5;
 	}
+	graph_pos.active = false;
+	graph_pos.radius = 0;
+	graph_pos.position = (Vector2) { GetScreenWidth()/2 -30, GetScreenHeight()/2 -30 };
+	graph_pos.speed = (Vector2) { 0, 0 };
 }
 
 static void Update(Camera2D camera, Rectangle textbox) {
-	if (CheckCollisionPointRec(GetMousePosition(), textbox)) textBox_active = true;
-	else textBox_active = false;
-
-	if (textBox_active) {
-		SetMouseCursor(MOUSE_CURSOR_IBEAM);
-		int key = GetCharPressed();
-		while (key > 0) {
-			if (( key >= 32) && (key <= 125) && (letterCount < max_chars)) {
-				name[letterCount] = (char)key;
-				name[letterCount+1] = '\0';
-				letterCount++;
+	if (graph) {
+		if (IsKeyPressed(KEY_TAB)) graph_pos.active = true;
+		if (graph_pos.active) {
+			if (IsKeyDown(KEY_UP)) {
+				graph_pos.position.y += GraphSpeed;
+			} else if (IsKeyDown(KEY_DOWN)) {
+				graph_pos.position.y -= GraphSpeed;
+			} else if (IsKeyDown(KEY_LEFT)) {
+				graph_pos.position.x -= GraphSpeed;
+			} else if (IsKeyDown(KEY_RIGHT)) {
+				graph_pos.position.x += GraphSpeed;
 			}
-			key = GetCharPressed();
 		}
-		if (IsKeyPressed(KEY_BACKSPACE)) {
-			letterCount--;
-			if (letterCount < 0) letterCount = 0;
-			name[letterCount] = '\0';
+	}
+	if (start) {
+		if (CheckCollisionPointRec(GetMousePosition(), textbox)) textBox_active = true;
+		else textBox_active = false;
+		if (textBox_active) {
+			SetMouseCursor(MOUSE_CURSOR_IBEAM);
+			int key = GetCharPressed();
+			while (key > 0) {
+				if (( key >= 32) && (key <= 125) && (letterCount < max_chars)) {
+					name[letterCount] = (char)key;
+					name[letterCount+1] = '\0';
+					letterCount++;
+				}
+				key = GetCharPressed();
+			}
+			if (IsKeyPressed(KEY_BACKSPACE)) {
+				letterCount--;
+				if (letterCount < 0) letterCount = 0;
+				name[letterCount] = '\0';
+			}
+		} else {
+			SetMouseCursor(MOUSE_CURSOR_DEFAULT);
 		}
-	} else {
-		SetMouseCursor(MOUSE_CURSOR_DEFAULT);
-	}
-	if (textBox_active) {
-		framesCounter++;
-	} else {
-		framesCounter = 0;
-	}
+		if (textBox_active) {
+			framesCounter++;
+		} else {
+			framesCounter = 0;
+		}
 
-	if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) {
-		Vector2 delta = GetMouseDelta();
-		delta = Vector2Scale(delta, -1.0f/camera.zoom);
-		camera.target = Vector2Add(camera.target, delta);
-	}
-	float wheel = GetMouseWheelMove();
-	if (wheel != 0) {
-		Vector2 mouseWorldPos = GetScreenToWorld2D(GetMousePosition(), camera);
-		camera.offset = GetMousePosition();
-		camera.target = mouseWorldPos;
-		const float zoomIncrement = 0.125;
-		camera.zoom += (wheel*zoomIncrement);
-		if (camera.zoom < zoomIncrement) camera.zoom = zoomIncrement;
+		if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) {
+			Vector2 delta = GetMouseDelta();
+			delta = Vector2Scale(delta, -1.0f/camera.zoom);
+			camera.target = Vector2Add(camera.target, delta);
+		}
+		float wheel = GetMouseWheelMove();
+		if (wheel != 0) {
+			Vector2 mouseWorldPos = GetScreenToWorld2D(GetMousePosition(), camera);
+			camera.offset = GetMousePosition();
+			camera.target = mouseWorldPos;
+			const float zoomIncrement = 0.125;
+			camera.zoom += (wheel*zoomIncrement);
+			if (camera.zoom < zoomIncrement) camera.zoom = zoomIncrement;
+		}
 	}
 }
 
 static void Draw(Camera2D camera, Rectangle textbox) {
 	BeginDrawing();
 		ClearBackground(RAYWHITE);
-		DrawRectangleRec(textbox, LIGHTGRAY);
-		if (textBox_active) DrawRectangleLines((int)textbox.x, (int)textbox.y, (int)textbox.width, (int)textbox.height, RED);
-		else DrawRectangleLines((int)textbox.x, (int)textbox.y, (int)textbox.width, (int)textbox.height, DARKGRAY);
+		if (start) {
+			DrawRectangleRec(textbox, LIGHTGRAY);
+			if (textBox_active) DrawRectangleLines((int)textbox.x, (int)textbox.y, (int)textbox.width, (int)textbox.height, RED);
+			else DrawRectangleLines((int)textbox.x, (int)textbox.y, (int)textbox.width, (int)textbox.height, DARKGRAY);
 
-		DrawText(name, (int)textbox.x + 5, (int)textbox.y + 8, 40, MAROON);
-		DrawText(TextFormat("INPUT CHARS: %i/%i", letterCount, max_chars), 315, 250, 20, DARKGRAY);
-		if (textBox_active) {
-			if (letterCount < max_chars) {
-				if (((framesCounter/20)%2) == 0) DrawText("_", (int)textbox.x + 8 + MeasureText(name, 40), (int)textbox.y + 12, 40, MAROON);
+			DrawText(name, (int)textbox.x + 5, (int)textbox.y + 8, 40, MAROON);
+			DrawText(TextFormat("INPUT CHARS: %i/%i", letterCount, max_chars), 315, 250, 20, DARKGRAY);
+			if (textBox_active) {
+				if (letterCount < max_chars) {
+					if (((framesCounter/20)%2) == 0) DrawText("_", (int)textbox.x + 8 + MeasureText(name, 40), (int)textbox.y + 12, 40, MAROON);
+				}
+				else DrawText("hello", 230, 300, 20, GRAY);
 			}
-			else DrawText("hello", 230, 300, 20, GRAY);
 		}
-		BeginMode2D(camera);
-			rlPushMatrix();
-				rlTranslatef(0,25*50,0);
-				rlRotatef(90,1,0,0);
-				DrawGrid(100,50);
-			rlPopMatrix();
-		EndMode2D();
+		if (graph) {
+			BeginMode2D(camera);
+				rlPushMatrix();
+					rlTranslatef(0,25*50,0);
+					rlRotatef(90,1,0,0);
+					DrawGrid(100,50);
+				rlPopMatrix();
+			EndMode2D();
+		}
 	EndDrawing();
 }
 
