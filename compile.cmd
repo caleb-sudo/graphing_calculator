ae99838f174a08a665dd35205aac0cdb4b0d3b93
caleb-sudo <123672600+caleb-sudo@users.noreply.github.com>
2023-12-22 08:42:04 -0700

    Update compile.cmd

--- compile.cmd
+++ compile.cmd
@@ -1,1 +1,1 @@
-gcc main.c -o main.exe -O1 -Wall -std=c99 -Wno-missing-braces -static -I include/ -L lib/ -lraylib -lopengl32 -lgdi32 -lwinmm
+gcc main.c -o main.exe -O1 -Wall -std=c99 -Wno-missing-braces -static -I include/ -L lib/ -lraylib -lopengl32 -lgdi32 -lwinmm -ldislin
