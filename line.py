052ca905dc4af7339d6ed4d2d4913e1a727d9750
caleb-sudo <123672600+caleb-sudo@users.noreply.github.com>
2023-12-28 16:29:39 -0700

    Update line.py

--- line.py
+++ line.py
@@ -1,7 +1,6 @@
 import plotly.express as px
 import ctypes
 graph = ctype.CDLL("lib/graph.dll")
-slope = ctype.c_int.in_dll(graph, "slope")
-x_int = ctype.c_int.in_dll(graph, "x_intercept")
-y_int = ctype.c_int.in_dll(graph, "y_intercept")
+x = ctype.c_int.dll(graph, "x")
+y = ctype.c_int.dll(graph, "y")
 fig.show();
