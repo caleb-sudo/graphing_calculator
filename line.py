4de37e31f281471c2ac8fdd29a31b02465d04b19
caleb-sudo <123672600+caleb-sudo@users.noreply.github.com>
2023-12-28 16:34:14 -0700

    Update line.py

--- line.py
+++ line.py
@@ -3,6 +3,6 @@ import numpy as np
 import pandas as pd
 import ctypes
 graph = ctypes.CDLL("lib/graph.dll")
-x = ctype.c_int.dll(graph, "x")
-y = ctype.c_int.dll(graph, "y")
+x = ctypes.c_int.dll(graph, "x")
+y = ctypes.c_int.dll(graph, "y")
 fig.show();
