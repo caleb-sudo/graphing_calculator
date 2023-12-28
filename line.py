e821a25c717207a2a873742dc52e445ec79f7858
caleb-sudo <123672600+caleb-sudo@users.noreply.github.com>
2023-12-28 16:33:49 -0700

    Update line.py

--- line.py
+++ line.py
@@ -2,7 +2,7 @@ import plotly.express as px
 import numpy as np
 import pandas as pd
 import ctypes
-graph = ctype.CDLL("lib/graph.dll")
+graph = ctypes.CDLL("lib/graph.dll")
 x = ctype.c_int.dll(graph, "x")
 y = ctype.c_int.dll(graph, "y")
 fig.show();
