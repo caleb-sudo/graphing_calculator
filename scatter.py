ce58c4b84b0a334f3e33381f7ac2306ff25f7c46
caleb-sudo <123672600+caleb-sudo@users.noreply.github.com>
2023-12-28 00:33:19 -0700

    Update scatter.py

--- scatter.py
+++ scatter.py
@@ -3,6 +3,5 @@ import ctypes
 graph = ctypes.CDLL("lib/graph.dll")
 x = ctypes.c_int.in_dll(graph, "x")
 y = ctypes.c_int.in_dll(graph, "y")
-z = ctypes.c_int.in_dll(graph, "z")
 fig = px.scatter(x = [x], y = [y]);
 fig.show()
