8d137e66e478464b6f4798076ad246f645f3f678
caleb-sudo <123672600+caleb-sudo@users.noreply.github.com>
2023-12-28 00:50:09 -0700

    Update line.py

--- line.py
+++ line.py
@@ -4,3 +4,4 @@ graph = ctype.CDLL("lib/graph.dll")
 slope = ctype.c_int.in_dll(graph, "slope")
 x_int = ctype.c_int.in_dll(graph, "x_intercept")
 y_int = ctype.c_int.in_dll(graph, "y_intercept")
+fig.show();
