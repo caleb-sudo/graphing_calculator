0993be2c20ec98da61c83139f648326bcf7c46dd
caleb-sudo <123672600+caleb-sudo@users.noreply.github.com>
2023-12-28 00:36:15 -0700

    Update 3d.py

--- 3d.py
+++ 3d.py
@@ -1,2 +1,6 @@
 import plotly.express as px
-from ctypes import CDLL
+import ctypes
+graph = ctypes.CDLL("lib/graph.dll")
+x = ctypes.c_int.in_dll(graph, "x")
+y = ctypes.c_int.in_dll(graph, "y")
+z = ctypes.c_int.in_dll(graph, "z")
