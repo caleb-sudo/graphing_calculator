cfe34a8ce99e95a66443c612a4f3decd7ff6b91b
caleb-sudo <123672600+caleb-sudo@users.noreply.github.com>
2023-12-30 14:34:11 -0700

    Delete scatter.py

--- scatter.py
+++
@@ -1,7 +0,0 @@
-import plotly.express as px
-import ctypes
-graph = ctypes.CDLL("lib/graph.dll")
-x = ctypes.c_int.in_dll(graph, "x")
-y = ctypes.c_int.in_dll(graph, "y")
-fig = px.scatter(x = [x], y = [y]);
-fig.show()
