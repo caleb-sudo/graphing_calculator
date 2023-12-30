45b244b2ab71fe8a547afb7959b430472a40e530
caleb-sudo <123672600+caleb-sudo@users.noreply.github.com>
2023-12-30 14:33:44 -0700

    Delete line.py

--- line.py
+++
@@ -1,8 +0,0 @@
-import plotly.express as px
-import numpy as np
-import pandas as pd
-import ctypes
-graph = ctypes.CDLL("lib/graph.dll")
-x = ctypes.c_int.dll(graph, "x")
-y = ctypes.c_int.dll(graph, "y")
-fig.show();
