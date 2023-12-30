dffac43ee96792445a815ce47f60bf089131ac42
caleb-sudo <123672600+caleb-sudo@users.noreply.github.com>
2023-12-30 14:33:34 -0700

    Delete 3d.py

--- 3d.py
+++
@@ -1,8 +0,0 @@
-import plotly.express as px
-import ctypes
-graph = ctypes.CDLL("lib/graph.dll")
-x = ctypes.c_int.in_dll(graph, "x")
-y = ctypes.c_int.in_dll(graph, "y")
-z = ctypes.c_int.in_dll(graph, "z")
-fig = px.scatter_3d(graph, x, y, z)
-fig.show()
