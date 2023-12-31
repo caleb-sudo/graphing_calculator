9c8d2ba8e453188b51f27c3352aefe598515944c
caleb-sudo <123672600+caleb-sudo@users.noreply.github.com>
2023-12-31 16:51:13 -0700

    Add files via upload

---
+++ histo.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import ctype
+
+plt.style.use('_mpl-gallary')
+
+variables = ctype.WinDLL("D://graphing_calculator/lib/mylib.dll")
+
+fig, ax = plt.subplots()
+plt.show()
