aa67638b14e972d263eac8097493d82f79542174
caleb-sudo <123672600+caleb-sudo@users.noreply.github.com>
2023-12-31 16:34:28 -0700

    Update 3d.py

--- 3d.py
+++ 3d.py
@@ -4,7 +4,7 @@ import ctypes
 
 plt.style.use('_mpl_gallery')
 
-variables = ctypes.WinDLL("D://graphing_calulator/lib/graph.dll")
+variables = ctypes.WinDLL("D://graphing_calulator/lib/mylib.dll")
 x = ctypes.c_int.in_dll(variables, "x")
 y = ctypes.c_int.in_dll(variables, "y")
 z = ctypes.c_int.in_dll(variables, "z")
