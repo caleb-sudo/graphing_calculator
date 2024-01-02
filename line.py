1fc18af042a4e7724d12a9bd5d4621d72e1ef153
caleb-sudo <123672600+caleb-sudo@users.noreply.github.com>
2024-01-02 16:20:54 -0700

    Add files via upload

---
+++ line.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import ctypes
+
+plt.style.use('_mpl-gallery')
+x = np.linspace(-10, 10, 999999999)
+variables = ctypes.WinDLL('D:/graphing_calc/lib/mydll.dll')
+m = ctypes.c_int.in_dll(variables, 'slope')
+b = ctypes.c_int.in_dll(variables, 'intercept')
+m = 1
+b = 2
+y = m*x + b
+
+fig, ax = plt.subplots()
+ax.scatter(x, y)
+plt.axis([-20, 20, -20, 20])
+plt.grid(True)
+plt.show()
