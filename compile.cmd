9c861a451a444536761c94fdf4f20c112559e00f
caleb-sudo <123672600+caleb-sudo@users.noreply.github.com>
2024-01-19 14:34:15 -0700

    Update compile.cmd

--- compile.cmd
+++ compile.cmd
@@ -1,1 +1,1 @@
-gcc main.c pbPlots.c supportLib.c -o main.exe -O1 -Wall -std=c99 -Wno-missing-braces -static -I include/ -L lib/ -lraylib -lopengl32 -lgdi32 -lwinmm -ldislin -lm
+gcc main.c -o main.exe -O1 -Wall -std=c99 -Wno-missing-braces -static -I include/ -L lib/ -lraylib -lopengl32 -lgdi32 -lwinmm
